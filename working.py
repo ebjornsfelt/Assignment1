325799a79068ec19d6d6dd4f3f5ab2165c9489d3
ebjornsfelt <ebjornsfelt@gmail.com>
2023-10-10 03:40:14 -0700

    Update working.py

--- working.py
+++ working.py
@@ -1,22 +1,23 @@
 import nltk
 
-grrr = "D:\SFU WORK\Fall 2023\SDA 250\Assignment1\Assignment1\FICTION-fantasy.txt"
-# A = "./Assignment1/FICTION-fantasy.txt"
-# B = "./Assignment1/FICTION-sci-fi.txt"
-# C = "./Assignment1/FICTION-thriller.txt"
+fantasyfile = "FICTION-fantasy.txt"
+scififile = "FICTION-sci-fi.txt"
+thrillerfile = "FICTION-thriller.txt"
 
-with open(grrr, 'r', encoding='utf-8') as file:
+with open(fantasyfile, 'r', encoding='utf-8') as file:
     groupA = file.read()
 
-# with open(B, 'r', encoding='utf-8') as file:
+with open(scififile, 'r', encoding='utf-8') as file:
     groupB = file.read()
     
-# with open(C, 'r', encoding='utf-8') as file:
+with open(thrillerfile, 'r', encoding='utf-8') as file:
     groupC = file.read()
     
 lengthA = len(groupA)
 lengthB = len(groupB)
 lengthC = len(groupC)
 
-
+# Question 1: Length of subgenre 
 print(lengthA)
+print(lengthB)
+print(lengthC)
