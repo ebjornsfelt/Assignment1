ad2ec36c65177bd7480fc382e9a86fbc906b0272
ebjornsfelt <ebjornsfelt@gmail.com>
2023-10-10 20:05:31 -0700

    Q2 & 3 with changes to Q1

--- working.py
+++ working.py
@@ -12,12 +12,27 @@ with open(scififile, 'r', encoding='utf-8') as file:
     
 with open(thrillerfile, 'r', encoding='utf-8') as file:
     groupC = file.read()
-    
+
+
+# Question 1: Length of subgenre 
 lengthA = len(groupA)
 lengthB = len(groupB)
 lengthC = len(groupC)
 
-# Question 1: Length of subgenre 
-print(lengthA)
-print(lengthB)
-print(lengthC)
+# Print the length of each subgenre
+print("Length of Group A:", lengthA)
+print("Length of Group B:", lengthB)
+print("Length of Group C:", lengthC)
+
+#Question 2: Lexical diversity
+# Tokenize the text into words
+tokensA = nltk.word_tokenize(groupA)
+tokensB = nltk.word_tokenize(groupB)
+tokensC = nltk.word_tokenize(groupC)
+
+# Calculate lexical diversity (unique words / total words)
+lexical_diversity_A = len(set(tokensA)) / len(tokensA)
+lexical_diversity_B = len(set(tokensB)) / len(tokensB)
+lexical_diversity_C = len(set(tokensC)) / len(tokensC)
+
+
