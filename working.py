d18957ec940044e544b769ea8a0ea513ac501fbb
ebjornsfelt <ebjornsfelt@gmail.com>
2023-10-11 22:34:43 -0700

    q1-3 done

--- working.py
+++ working.py
@@ -17,7 +17,44 @@ lengthA = len(groupA)
 lengthB = len(groupB)
 lengthC = len(groupC)
 
+<<<<<<< Updated upstream
 # Question 1: Length of subgenre 
 print(lengthA)
 print(lengthB)
 print(lengthC)
+=======
+# Print the length of each subgenre
+print("Length of Group A:", lengthA)
+print("Length of Group B:", lengthB)
+print("Length of Group C:", lengthC)
+
+#Question 2: Lexical diversity
+# Tokenize the text excluding the repeating words.
+wordsA = sorted(set(w.lower() for w in groupA if w.isalpha()))
+wordsB = sorted(set(w.lower() for w in groupB if w.isalpha()))
+wordsC = sorted(set(w.lower() for w in groupC if w.isalpha()))
+
+lexical_diversity_A = len(wordsA) / len(groupA.split())
+lexical_diversity_B = len(wordsB) / len(groupB.split())
+lexical_diversity_C = len(wordsC) / len(groupC.split())
+
+print("Lexical diversity Group A:", lexical_diversity_A)
+print("Lexical diversity Group B:", lexical_diversity_B)
+print("Lexical diversity Group C:", lexical_diversity_C)
+
+#Question 3: 10 most frequent words
+# Tokenize without excluding the repeating words.
+wordsAnonunique = groupA.lower().split()
+wordsBnonunique = groupB.lower().split()
+wordsCnonunique = groupC.lower().split()
+
+fdistA = FreqDist(wordsAnonunique)
+fdistB = FreqDist(wordsBnonunique)
+fdistC = FreqDist(wordsCnonunique)
+
+print("The 10 most common words and their count are:", fdistA.most_common(10))
+print("The 10 most common words and their count are:", fdistB.most_common(10))
+print("The 10 most common words and their count are:", fdistC.most_common(10))
+
+#Question 4
+>>>>>>> Stashed changes
